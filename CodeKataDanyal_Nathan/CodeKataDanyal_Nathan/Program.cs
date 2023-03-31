a4551d0f733ed4aacaec6467212426e5778302a0
Danyal <86931465+danymcdan0@users.noreply.github.com>
2023-03-31 17:23:20 +0100

    Completed method and test cases!

--- CodeKataDanyal_Nathan/CodeKataDanyal_Nathan/Program.cs
+++ CodeKataDanyal_Nathan/CodeKataDanyal_Nathan/Program.cs
@@ -10,40 +10,51 @@ namespace CodeKataDanyal_Nathan
         }
         //TODO
         //Given a amount of money, return the number of bills/coins of each denomination required.Use the lowest possible total number of bills and coins.
-        //Modulus
-        //
         public static string MoneyProblem(string money) {
             int twenty = 0, ten = 0, five = 0, two = 0, one = 0, fiftyP = 0, twentyP = 0, tenP = 0, fiveP = 0, oneP = 0;
-            int index = money.IndexOf(".");
-            int maxLength = money.Length - 1;
-            Console.WriteLine(index);
-            int wholeNumber = Convert.ToInt32(money.Substring(0, index));
-
-            while (wholeNumber > 0)
+            if (!money.Contains("."))
             {
-                twenty = wholeNumber / 20;
-                wholeNumber -= (twenty * 20);
-                ten = wholeNumber / 10;
-                wholeNumber -= (ten * 10);
-                five = wholeNumber / 5;
-                wholeNumber -= (five * 5);
-                two = wholeNumber / 2;
-                wholeNumber -= (two * 2);
-                one = wholeNumber;
-                wholeNumber -= (one);
+                throw new ArgumentException("Incorrect format -> Needs to be in decimals e.g. 00.00");
             }
+            if (Convert.ToDouble(money) > 0)
+            {
+                int index = money.IndexOf(".");
+                int maxLength = money.Length - 1;
+                int wholeNumber = Convert.ToInt32(money.Substring(0, index));
+                Console.WriteLine(money[0] + " " + money[index + 1]);
+                Console.WriteLine(money[index + 1] + " " + money[maxLength]);
 
-            double decimalNumber = Convert.ToDouble(money);
+                while (wholeNumber > 0)
+                {
+                    twenty = wholeNumber / 20;
+                    wholeNumber -= (twenty * 20);
+                    ten = wholeNumber / 10;
+                    wholeNumber -= (ten * 10);
+                    five = wholeNumber / 5;
+                    wholeNumber -= (five * 5);
+                    two = wholeNumber / 2;
+                    wholeNumber -= (two * 2);
+                    one = wholeNumber;
+                    wholeNumber -= (one);
+                }
 
-            var onlyDecimal = decimalNumber - wholeNumber;
+                int decimalNumber = Convert.ToInt32(money.Substring(index + 1));
 
-
-
-            //Console.WriteLine(decimalNumber);
-
+                while (decimalNumber > 0)
+                {
+                    fiftyP = decimalNumber / 50;
+                    decimalNumber -= (fiftyP * 50);
+                    twentyP = decimalNumber / 20;
+                    decimalNumber -= (twentyP * 20);
+                    tenP = decimalNumber / 10;
+                    decimalNumber -= (tenP * 10);
+                    fiveP = decimalNumber / 5;
+                    decimalNumber -= (fiveP * 5);
+                    oneP = decimalNumber;
+                    decimalNumber -= (oneP);
+                }
+            }
             return $"$20: {twenty}, $10: {ten}, $5: {five}, $2: {two}, $1: {one}, $0.50: {fiftyP}, $0.20: {twentyP}, $0.10: {tenP}, $0.05: {fiveP}, $0.01: {oneP}";
         }
-
-        
     }
 }
--- CodeKataDanyal_Nathan/MoneyProblemTests/MoneyProblem.cs
+++ CodeKataDanyal_Nathan/MoneyProblemTests/MoneyProblem.cs
@@ -1,18 +1,26 @@
 using CodeKataDanyal_Nathan;
+using System.Diagnostics;
+
 namespace MoneyProblemTests
 {
     public class Tests
     {
-        [TestCase("5.47", "$20: 0, $10: 0, $5: 1, $2: 0, $1: 0, $0.50: 0, $0.20: 2, $0.10: 0, $0.05: 2, $0.01: 2")]
-        [TestCase("20.80", "$20: 1, $10: 0, $5: 1, $2: 0, $1: 0, $0.50: 1, $0.20: 1, $0.10: 1, $0.05: 0, $0.01: 0")]
-        [TestCase("1", "$20: 0, $10: 0, $5: 0, $2: 0, $1: 1, $0.50: 0, $0.20: 0, $0.10: 0, $0.05: 0, $0.01: 0")]
-        [TestCase("0", "$20: 0, $10: 0, $5: 0, $2: 0, $1: 0, $0.50: 0, $0.20: 0, $0.10: 0, $0.05: 0, $0.01: 0")]
-        [TestCase("100", "$20: 5, $10: 0, $5: 0, $2: 0, $1: 0, $0.50: 0, $0.20: 0, $0.10: 0, $0.05: 0, $0.01: 0")]
+        [TestCase("5.47", "$20: 0, $10: 0, $5: 1, $2: 0, $1: 0, $0.50: 0, $0.20: 2, $0.10: 0, $0.05: 1, $0.01: 2")]
+        [TestCase("20.80", "$20: 1, $10: 0, $5: 0, $2: 0, $1: 0, $0.50: 1, $0.20: 1, $0.10: 1, $0.05: 0, $0.01: 0")]
+        [TestCase("0.00", "$20: 0, $10: 0, $5: 0, $2: 0, $1: 0, $0.50: 0, $0.20: 0, $0.10: 0, $0.05: 0, $0.01: 0")]
+        [TestCase("100.00", "$20: 5, $10: 0, $5: 0, $2: 0, $1: 0, $0.50: 0, $0.20: 0, $0.10: 0, $0.05: 0, $0.01: 0")]
         [TestCase("0.50", "$20: 0, $10: 0, $5: 0, $2: 0, $1: 0, $0.50: 1, $0.20: 0, $0.10: 0, $0.05: 0, $0.01: 0")]
         [TestCase("38.86", "$20: 1, $10: 1, $5: 1, $2: 1, $1: 1, $0.50: 1, $0.20: 1, $0.10: 1, $0.05: 1, $0.01: 1")]
         public void GivenMoney_GetValue_ReturnExpectedString(string money, string expected)
         {
             Assert.That(expected, Is.EqualTo(Program.MoneyProblem(money)));
+        }
+        [TestCase("1")]
+        [TestCase("-1")]
+        [TestCase("0")]
+        public void GivenMoney_GetIncorrectFormat_ReturnException(string money)
+        {
+            Assert.That(() => Program.MoneyProblem(money), Throws.TypeOf<ArgumentException>());
         }
     }
 }
