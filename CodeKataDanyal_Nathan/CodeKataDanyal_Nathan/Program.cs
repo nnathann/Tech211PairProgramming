6b7f2dd89a750448b44ca3b3d1333e62baeeffc8
Danyal <86931465+danymcdan0@users.noreply.github.com>
2023-03-31 15:10:47 +0100

    Added base Unit tests and Money Problem method

---
+++ CodeKataDanyal_Nathan/CodeKataDanyal_Nathan/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace CodeKataDanyal_Nathan
+{
+    public class Program
+    {
+        static void Main()
+        {
+        }
+        //TODO
+        //Given a amount of money, return the number of bills/coins of each denomination required.Use the lowest possible total number of bills and coins.
+        //Modulus
+        //
+        public static string MoneyProblem(double money) {
+            int twenty = 0, ten = 0, five = 0, two = 0, one = 0, fiftyP = 0, twentyP = 0, tenP = 0, fiveP = 0, oneP = 0;
+
+            return $"$20: {twenty}, $10: {ten}, $5: {five}, $2: {two}, $1: {one}, $0.50: {fiftyP}, $0.20: {twentyP}, $0.10: {tenP}, $0.05: {fiveP}, $0.01: {oneP}";
+        }
+    }
+}
