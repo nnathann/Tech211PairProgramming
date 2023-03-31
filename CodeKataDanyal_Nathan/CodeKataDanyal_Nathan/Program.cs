b68acd2002329aa0a9b3c66e4599fddd6bf2b525
Danyal <86931465+danymcdan0@users.noreply.github.com>
2023-03-31 15:54:10 +0100

    Added loop to calculate whole number

--- CodeKataDanyal_Nathan/CodeKataDanyal_Nathan/Program.cs
+++ CodeKataDanyal_Nathan/CodeKataDanyal_Nathan/Program.cs
@@ -6,6 +6,7 @@ namespace CodeKataDanyal_Nathan
     {
         static void Main()
         {
+            Console.WriteLine(MoneyProblem("43.32"));
         }
         //TODO
         //Given a amount of money, return the number of bills/coins of each denomination required.Use the lowest possible total number of bills and coins.
@@ -13,13 +14,30 @@ namespace CodeKataDanyal_Nathan
         //
         public static string MoneyProblem(string money) {
             int twenty = 0, ten = 0, five = 0, two = 0, one = 0, fiftyP = 0, twentyP = 0, tenP = 0, fiveP = 0, oneP = 0;
+            int index = money.IndexOf(".");
+            int maxLength = money.Length - 1;
+            Console.WriteLine(index);
+            int wholeNumber = Convert.ToInt32(money.Substring(0, index));
 
-            string wholenumber = money[0]: money.IndexOf(".");
-            
+            while (wholeNumber > 0)
+            {
+                twenty = wholeNumber / 20;
+                wholeNumber -= (twenty * 20);
+                ten = wholeNumber / 10;
+                wholeNumber -= (ten * 10);
+                five = wholeNumber / 5;
+                wholeNumber -= (five * 5);
+                two = wholeNumber / 2;
+                wholeNumber -= (two * 2);
+                one = wholeNumber;
+                wholeNumber -= (one);
+            }
+
+            //int decimalNumber = Convert.ToInt32(money.Substring(index, maxLength));
+            //Console.WriteLine(decimalNumber);
+
             return $"$20: {twenty}, $10: {ten}, $5: {five}, $2: {two}, $1: {one}, $0.50: {fiftyP}, $0.20: {twentyP}, $0.10: {tenP}, $0.05: {fiveP}, $0.01: {oneP}";
         }
-
-        //given 43
 
         
     }
