0001220114826eceddfd1b3eff58ac4e7a5fde0f
nnathann <nathanyousee@gmail.com>
2023-03-31 15:34:25 +0100

    Changed double to string

--- CodeKataDanyal_Nathan/CodeKataDanyal_Nathan/Program.cs
+++ CodeKataDanyal_Nathan/CodeKataDanyal_Nathan/Program.cs
@@ -11,10 +11,16 @@ namespace CodeKataDanyal_Nathan
         //Given a amount of money, return the number of bills/coins of each denomination required.Use the lowest possible total number of bills and coins.
         //Modulus
         //
-        public static string MoneyProblem(double money) {
+        public static string MoneyProblem(string money) {
             int twenty = 0, ten = 0, five = 0, two = 0, one = 0, fiftyP = 0, twentyP = 0, tenP = 0, fiveP = 0, oneP = 0;
 
+            string wholenumber = money[0]: money.IndexOf(".");
+            
             return $"$20: {twenty}, $10: {ten}, $5: {five}, $2: {two}, $1: {one}, $0.50: {fiftyP}, $0.20: {twentyP}, $0.10: {tenP}, $0.05: {fiveP}, $0.01: {oneP}";
         }
+
+        //given 43
+
+        
     }
 }
